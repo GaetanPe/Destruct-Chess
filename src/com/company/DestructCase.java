72bc48ba68c64d3d0f93777d9061f4598e76c6c5
Killian Vendewinkele <killian.vendewinkele@gmail.com>
2021-09-22 13:30:47 +0200

    Amelioration de la destruction de case

--- src/com/company/DestructCase.java
+++ src/com/company/DestructCase.java
@@ -7,14 +7,12 @@ public class DestructCase {
     static int verifCaseX(){
         Scanner destructX = new Scanner(System.in);
         System.out.println("Choisissez la coordonnée x de la case à détruire.");
-        int detruitCaseX = destructX.nextInt();
-        return detruitCaseX;
+        return destructX.nextInt();
     }
     static int verifCaseY(){
         Scanner destructY = new Scanner(System.in);
         System.out.println("Choisissez la coordonnée y de la case à détruire.");
-        int detruitCaseY = destructY.nextInt();
-        return detruitCaseY;
+        return destructY.nextInt();
     }
 
     public static void main() {
@@ -26,18 +24,18 @@ public class DestructCase {
         int destructCaseY = destructY.nextInt();
 
         while(grille[destructCaseX][destructCaseY] == grille[pion1x][pion1y] || grille[destructCaseX][destructCaseY] == grille[pion2x][pion2y]){
-            verifCaseX();
-            verifCaseY();
+            destructCaseX = verifCaseX();
+            destructCaseY = verifCaseY();
         }
 
         while(destructCaseX > 10 || destructCaseX < 0 || destructCaseY > 9 || destructCaseY < 0 ) {
-            verifCaseX();
-            verifCaseY();
+            destructCaseX = verifCaseX();
+            destructCaseY = verifCaseY();
         }
 
         while(grille[destructCaseX][destructCaseY] == "[x]"){
-            verifCaseX();
-            verifCaseY();
+            destructCaseX = verifCaseX();
+            destructCaseY = verifCaseY();
         }
 
         grille[destructCaseX][destructCaseY] == "[x]";
